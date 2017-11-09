6d4606326d12b1184a3af1b99942be8a972ef6ef
Vadim Prokopev <vadimjprokopev@gmail.com>
2017-11-09 19:03:49 +0300

    Made deck generate method with cycle

--- src/main/java/com/tianara/hsArenaGen/DeckGenerator.java
+++ src/main/java/com/tianara/hsArenaGen/DeckGenerator.java
@@ -1,6 +1,7 @@
 package com.tianara.hsArenaGen;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -8,76 +9,36 @@ import java.util.Set;
 
 public class DeckGenerator {
 	
-	int common;
-	int rare;
-	int epic;
-	int legendary;
-	
+	private Map<String, Integer> rarityAmount = new HashMap<>();
 	private Map<String, Map<String, List<Card>>> cards;
 	
 	//Determined by fair die roll :3
 	public void setRarities() {
-		common = 13;
-		rare = 9;
-		epic = 5;
-		legendary = 3;
+		rarityAmount.put("Common", 13);
+		rarityAmount.put("Rare", 9);
+		rarityAmount.put("Epic", 5);
+		rarityAmount.put("Legendary", 3);
 	}
 	
 	public List<Card> generate(String clas) {
-		List<Card> commonCards = cards.get("Basic").get(clas);
-		commonCards.addAll(cards.get("Basic").get("Neutral"));
-		commonCards.addAll(cards.get("Common").get(clas));
-		commonCards.addAll(cards.get("Common").get("Neutral"));
-		
-		List<Card> rareCards = cards.get("Rare").get(clas);
-		commonCards.addAll(cards.get("Rare").get("Neutral"));
-		
-		List<Card> epicCards = cards.get("Epic").get(clas);
-		commonCards.addAll(cards.get("Epic").get("Neutral"));
-		
-		List<Card> legendaryCards = cards.get("Legendary").get(clas);
-		commonCards.addAll(cards.get("Legendary").get("Neutral"));
-		
-		Set<Integer> commonIndices = new HashSet<>();
-		
-		while(commonIndices.size() != common) {
-			commonIndices.add((int)(Math.random() * (double)commonCards.size()));
-		}
-		
-		Set<Integer> rareIndices = new HashSet<>();
-		
-		while(rareIndices.size() != common) {
-			rareIndices.add((int)(Math.random() * (double)rareCards.size()));
-		}
-		
-		Set<Integer> epicIndices = new HashSet<>();
-		
-		while(epicIndices.size() != common) {
-			epicIndices.add((int)(Math.random() * (double)epicCards.size()));
-		}
-		
-		Set<Integer> legendaryIndices = new HashSet<>();
-		
-		while(legendaryIndices.size() != common) {
-			legendaryIndices.add((int)(Math.random() * (double)legendaryCards.size()));
-		}
 		
 		List<Card> deck = new ArrayList<>();
 		
-		for(int index : commonIndices) {
-			deck.add(commonCards.get(index));
-		}
+		for(String rarity : new String[] {"Common", "Rare", "Epic", "Legendary"}) {
+			List<Card> rarityCards = new ArrayList<>();
+			
+			rarityCards.addAll(cards.get(rarity).get(clas));
+			rarityCards.addAll(cards.get(rarity).get("Neutral"));
+			
+			Set<Integer> indices = new HashSet<>();
+			
+			while(indices.size() != rarityAmount.get(rarity)) {
+				indices.add((int)(Math.random() * (double)(rarityCards.size() - 1)));
+			}
 
-		for(int index : rareIndices) {
-			deck.add(rareCards.get(index));
-		}
-
-		for(int index : epicIndices) {
-			deck.add(epicCards.get(index));
-		}
-
-		for(int index : legendaryIndices) {
-			deck.add(legendaryCards.get(index));
+			for(int index : indices) {
+				deck.add(rarityCards.get(index));
+			}
 		}
 		
 		return deck;
